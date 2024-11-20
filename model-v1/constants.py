0a19a020d8e1056b5bc958403b6424fe54ef72c1
Dylan Simpson <dylancsimpson@gmail.com>
2024-11-20 17:51:51 +0000

    feat: adding simulator

--- model-v1/constants.py
+++ model-v1/constants.py
@@ -8,10 +8,4 @@ METADATA_PATH = "metadata.pkl"
 CLASSES = {
     0: "rest", #saftey
     1: "closed_fist", #go up
-    2: "open_palm", #go down
-    3: "ok", #go forwards
-    4: "pointer_finger", #go backwards
-    5: "peace", #roatate left
-    6: "shaaa", #rotate right
-    7: "peace_among_worlds", #do a flip
 }
